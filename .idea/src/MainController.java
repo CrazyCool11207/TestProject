c2d4b53992d2582cf92f593270ed1b019239e040
deadHorse <mathiasschulte@aol.com>
2017-03-06 23:51:57 +0100

    Hopefully nothing broke >.<

---
+++ .idea/src/MainController.java
@@ -0,0 +1,16 @@
+package src;
+
+/**
+ * Created by MissingNo on 06.03.2017.
+ */
+public class MainController {
+	public static void main(String[] args){
+		new MainController();
+	}
+	
+	
+	public MainController(){
+				
+	}
+	
+}
