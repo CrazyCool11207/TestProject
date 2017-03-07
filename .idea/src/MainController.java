8c40e708d088a5d5c822077cd150b604613c8ee3
deadHorse <mathiasschulte@aol.com>
2017-03-07 20:37:07 +0100

    A

--- .idea/src/MainController.java
+++ .idea/src/MainController.java
@@ -8,9 +8,20 @@ public class MainController {
 		new MainController();
 	}
 	
-	
 	public MainController(){
-				
+		long initialTime = System.nanoTime();
+		final double timeU = 1000000000 / UPS;
+		final double timeF = 1000000000 / FPS;
+		double deltaU = 0, deltaF = 0;
+		int frames = 0, ticks = 0;
+		long timer = System.currentTimeMillis();
+			}
+			
+			
+			
+			
+			
+		}
 	}
 	
 }
